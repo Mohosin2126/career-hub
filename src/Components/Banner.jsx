cd2b67d4fd55b5dbff82bcca6646c4741ce9a38c
Mohosin212601~ <mohosin2126@gmail.com>
2024-02-21 21:28:15 +0600

    added banner section

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -3,7 +3,15 @@
 const Banner = () => {
     return (
         <div>
-            <h1>hello this is me banner section</h1>
+           <div className="flex ">
+<div className="flex-1">
+    <h1 className="font-serif text-center mt-28 text-5xl"> One Step <br /> Closer To <br />
+    <span className="text-blue-500">  Dream Job </span></h1>
+</div>
+           <div className="flex-1">
+            <img src="../../public/assets/images/user.png" alt="" />
+           </div>
+           </div>
         </div>
     );
 };
