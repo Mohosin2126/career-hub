0467e80ec40ee9967fe6cea98af7ae430a8d0311
Mohosin212601~ <mohosin2126@gmail.com>
2024-02-21 21:33:03 +0600

    added paragraph in banner and designed

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -5,8 +5,9 @@ const Banner = () => {
         <div>
            <div className="flex ">
 <div className="flex-1">
-    <h1 className="font-serif text-center mt-28 text-5xl"> One Step <br /> Closer To <br />
+    <h1 className="font-serif  mt-28 text-5xl font-extrabold"> One Step <br /> Closer To <br />
     <span className="text-blue-500">  Dream Job </span></h1>
+    <p className="mt-5"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae optio similique molestias. Perferendis tempora similique nesciunt veritatis recusandae dolore culpa sequi delectus dignissimos minus quibusdam esse temporibus, labore itaque odit!</p>
 </div>
            <div className="flex-1">
             <img src="../../public/assets/images/user.png" alt="" />
